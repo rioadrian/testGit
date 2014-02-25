604f27f9be08a20f41df025fcb6d16a263a3d319
rioadrian <ryo.adrian@yahoo.com>
2014-02-25 20:33:33 +0700

    test commit kedua

--- testGit/ViewController.m
+++ testGit/ViewController.m
@@ -19,7 +19,8 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    NSLog(@"Halo");
+    NSLog(@"Halo world");
+    
 }
 
 - (void)didReceiveMemoryWarning
